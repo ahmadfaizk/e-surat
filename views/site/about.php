45c6057ca365692152e9eae5f36a316efda19834
ahmadfaizk <ach.faiz.k@gmail.com>
2019-11-04 14:20:04 +0700

    make with gii

---
+++ views/site/about.php
@@ -0,0 +1,18 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'About';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-about">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <h3>Nama : Ahmad Faiz Kamaludin</h3>
+        <h3>NRP : 2103181047</h3>
+        <h3>Kelas : 2 D3 Teknik Informatika B</h3>
+    </p>
+</div>
